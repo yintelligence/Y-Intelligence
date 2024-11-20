5d81e94d25a5e93ae0ccd4c22ee04894bc8413a0
LM Yudhy Prayitno <119650090+Yudhy-code-stranger@users.noreply.github.com>
2024-11-21 03:36:16 +0800

    fix:url post

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fuwari.vercel.app/',
+  site: 'https://y-intelligence.vercel.app/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
